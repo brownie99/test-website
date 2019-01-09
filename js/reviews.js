2e5cac8e1f3db940ddbb84caee3183e72e5f8fb6
Scott Brown <scurry99@hotmail.co.uk>
2019-01-09 00:41:16 +0000

    CSSSSSSS

--- js/reviews.js
+++ js/reviews.js
@@ -1,18 +1,45 @@
-var reviewsJson = $.getJSON("https://cors-anywhere.herokuapp.com/https://store.steampowered.com/appreviews/105600?json=1&key=95E80E9F4247BA92759FB703C4B16FC2", function(data){
-		console.log(data);
-		console.log(data.reviews[0].review);
-		$("#review1").html(data.reviews[0].review);
-	});
+
 var reviewsString; 
 
 
 $(document).ready(function(){
-
+	var reviewsJson = $.getJSON("https://cors-anywhere.herokuapp.com/https://store.steampowered.com/appreviews/105600?json=1&key=95E80E9F4247BA92759FB703C4B16FC2", function(data){
+	var htmlString = "";
+	$.each(reviewsJson.responseJSON.reviews, function(index, element){
+		htmlString += "<div class=\"reviewDiv\"><p>" + element.review + "</p></div>";
+	});
+	$('.bxslider').html(htmlString);
+	var maxHeight = 20;
 	console.log("init fired");
-	console.log(reviewsString);
-      $('.bxslider').bxSlider({
-		  made: 'fade',
-		  captions: false,
-		  slideWidth: 800
-	  });
+	var sliderWidth = $(window).width()*0.7;
+	$('.bxslider p').each(function(){
+		console.log($(this).height());
+		if ($(this).height() > maxHeight){
+			maxHeight = $(this).height();
+		}
+	});
+    $('.bxslider').bxSlider({
+			made: 'fade',
+			captions: false,
+			slideWidth: sliderWidth
+		});
+	console.log(maxHeight);
+	$('.bx-viewport').height(maxHeight);
+	maxHeight = 20;
+	$('.bxslider p').each(function(){
+		console.log($(this).height());
+		if ($(this).height() > maxHeight){
+			maxHeight = $(this).height();
+		}
+	});
+	console.log(maxHeight);
+	$('.bx-viewport').height(maxHeight + 40);
+	
+	
+	
+		
+	});
+	
+
+		
     });
